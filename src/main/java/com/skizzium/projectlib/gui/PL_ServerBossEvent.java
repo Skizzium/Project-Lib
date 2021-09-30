9445cd6b7a52d39d05dbf3119dc0cfc453df8f4c
uSkizzik <58179002+uSkizzik@users.noreply.github.com>
2021-09-30 19:36:11 +0300

    Minor Adjustments

--- src/main/java/com/skizzium/projectlib/gui/PL_ServerBossEvent.java
+++ src/main/java/com/skizzium/projectlib/gui/PL_ServerBossEvent.java
@@ -30,7 +30,11 @@ public class PL_ServerBossEvent extends PL_BossEvent {
             }
         }
     }
-    
+
+    /**
+     * Changes the boss bar's display name.
+     * @param newName The new display name. I recommend using a TextComponent for this.
+     */
     public void setName(Component newName) {
         if (!Objects.equal(newName, this.name)) {
             super.setName(newName);
@@ -38,7 +42,7 @@ public class PL_ServerBossEvent extends PL_BossEvent {
         }
     }
 
-    public void setColor(PL_BossBarColor color) {
+    public void setColor(PL_BossEvent.PL_BossBarColor color) {
         if (color != this.color) {
             super.setColor(color);
             this.broadcast(new PL_UpdateStyleBossEventPacket(this));
@@ -52,6 +56,9 @@ public class PL_ServerBossEvent extends PL_BossEvent {
         }
     }
 
+    /**
+     * Sets if the sky should get darkened when the bar is rendered in. This function is also utilized by the Wither.
+     */
     public PL_BossEvent setDarkenScreen(boolean flag) {
         if (flag != this.darkenScreen) {
             super.setDarkenScreen(flag);
@@ -67,20 +74,30 @@ public class PL_ServerBossEvent extends PL_BossEvent {
         }
         return this;
     }
-    
+
+    /**
+     * Sets the bar's progress.
+     * @param f This is the progress you want to set your bar to. It must be a float between 0.0F and 1.0F. You can calculate your bar's progress by dividing your boss's current health by it's max health.
+     */
     public void setProgress(float f) {
         if (f != this.progress) {
             super.setProgress(f);
             this.broadcast(new PL_UpdateProgressBossEventPacket(this));
         }
     }
-    
+
+    /**
+     * This method adds the given player to the list of players that are rendering this boss bar and sends the player a packet to render in the boss bar
+     */
     public void addPlayer(ServerPlayer player) {
         if (this.players.add(player) && this.visible) {
             PL_PacketRegistry.INSTANCE.sendTo(new PL_AddBossEventPacket(this), player.connection.getConnection(), NetworkDirection.PLAY_TO_CLIENT);
         }
     }
 
+    /**
+     * This method removes the given player to the list of players that are rendering this boss bar and sends the player a packet to unrender in the boss bar
+     */
     public void removePlayer(ServerPlayer player) {
         if (this.players.remove(player) && this.visible) {
             PL_PacketRegistry.INSTANCE.sendTo(new PL_RemoveBossEventPacket(this), player.connection.getConnection(), NetworkDirection.PLAY_TO_CLIENT);
