c57e48acbd18458e06a753a4820645ca9426eb4f
uSkizzik <58179002+uSkizzik@users.noreply.github.com>
2021-11-04 13:55:05 +0200

    WIP

--- src/main/java/com/skizzium/projectlib/init/PL_Comamnds.java
+++ src/main/java/com/skizzium/projectlib/init/PL_Comamnds.java
@@ -1,11 +1,8 @@
 package com.skizzium.projectlib.init;
 
-import com.mojang.brigadier.CommandDispatcher;
 import com.skizzium.projectlib.ProjectLib;
 import com.skizzium.projectlib.commands.CommandReplacement;
 import com.skizzium.projectlib.commands.PL_BossBarCommands;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.Commands;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -13,11 +10,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = ProjectLib.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class PL_Comamnds {
     @SubscribeEvent
-    public void onRegisterCommands(RegisterCommandsEvent event) {
-        final CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
-
-        if (event.getEnvironment() != Commands.CommandSelection.INTEGRATED) {
-            CommandReplacement.replaceAndRegister(dispatcher, PL_BossBarCommands.getNode());
-        }
+    public static void registerCommands(RegisterCommandsEvent event) {
+        CommandReplacement.replaceAndRegister(event.getDispatcher(), PL_BossBarCommands.getNode());
     }
 }
